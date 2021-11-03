89663952da8a283008454252f122f5af9f2f928c
phariven <harivenkatesh_polnati@optum.com>
2021-11-03 11:32:15 +0530

    Autoconfigurations, Autowired and Basic secuiryt

---
+++ src/main/java/com/springtrainings/drugapp/service/customconfiguration/DrugServiceConfiguration.java
@@ -0,0 +1,16 @@
+package com.springtrainings.drugapp.service.customconfiguration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.springtrainings.drugapp.service.DrugService;
+
+@Configuration
+public class DrugServiceConfiguration {
+
+	@Bean
+	public DrugService drugServiceObj() {
+		return new DrugService();
+	}
+
+}
